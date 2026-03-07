ec37a3f7681c33576fdaf25f3bdc27f39ac4993f
Tran Tuan Minh <ming18380@gmail.com>
2026-03-07 11:32:10 +0700

    feat(test): update file test_ping (reload ci/cd (2))

---
+++ tests/test_ping.py
@@ -0,0 +1,12 @@
+import pytest
+from unittest.mock import AsyncMock, MagicMock
+from bot import ping
+
+@pytest.mark.asyncio
+async def test_ping():
+    ctx = MagicMock()
+    ctx.send = AsyncMock()
+
+    await ping(ctx)
+
+    ctx.send.assert_called_once_with("Pong!")
