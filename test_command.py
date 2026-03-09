eb7af519b83623734dfcbb6eeb40095e19058651
Tran Tuan Minh <ming18380@gmail.com>
2026-03-09 13:12:17 +0700

    feat(test): add sonar test command (2)

--- test_command.py
+++ test_command.py
@@ -2,7 +2,10 @@ import discord
 from discord.ext import commands
 import os
 
-bot = commands.Bot(command_prefix="!")
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 # hardcoded password (Sonar sẽ cảnh báo security)
 PASSWORD = "123456"
