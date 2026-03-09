05685f51766656d8edfc8a3d13b088e8589e3356
Tran Tuan Minh <ming18380@gmail.com>
2026-03-09 14:29:55 +0700

    feat(test): add test for avoiding uncovering to pass test SonarQube (2)

--- bad_command.py
+++ bad_command.py
@@ -2,7 +2,10 @@ import os
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="!")
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents  )
 
 # lấy token từ environment variable (an toàn hơn)
 TOKEN = os.getenv("DISCORD_TOKEN")
--- test_command.py
+++ test_command.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 from bad_command import calculate_sum
-import os
+
 
 intents = discord.Intents.default()
 intents.message_content = True
